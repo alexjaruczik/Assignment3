a255fad1a5fd52e5ac95d631dcad30e6e260d32f
alexjaruczik <alex.jaruczik@ontariotechu.net>
2025-11-20 15:35:57 -0500

    Created partials and set up some bootstrap designs.

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'HollywoodStar' });
 });
 
+router.get('/reviews', function(req, res, next) {
+  res.render('reviews', { title: 'HollywoodStar' });
+});
+
 module.exports = router;
