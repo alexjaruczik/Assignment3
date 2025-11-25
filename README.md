d80f0c569ecfcc23aa7044ecda060d0744f800b2
alexjaruczik <alex.jaruczik@ontariotechu.net>
2025-11-25 16:06:49 -0500

    Updated cache to properly respect .gitignore

--- README.md
+++
@@ -1,2 +0,0 @@
-# Assignment3
-All stylesheets are imported from Bootstrap, and icons were imported from FontAwesome.
--- server/config/db.js
+++
@@ -1,2 +0,0 @@
-// URI to my MongoDB database.
-module.exports={"URI":"mongodb+srv://alexjaruczik:MZFEkSWKtduI2Vg5@cluster0.hjxceqh.mongodb.net/reviews"}
